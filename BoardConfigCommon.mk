90e99de9dda2eb299e4b6b37ab4c0885dc3a3fd6
Abdalrahman Eissa <abdalrahmaneissaa@proton.me>
2026-03-29 10:18:46 +0200

    sm8750-common: Relocate system properties to dedicated directory
    Move vendor.prop, odm.prop, product.prop, and system_ext.prop into a new properties/ subdirectory. Updating the BoardConfigCommon.mk paths to reflect this change ensures that all partition-specific property overrides are centrally managed in one location, further decluttering the root device tree for the (sun) platform.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,10 +63,10 @@ BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Properties
-TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
+TARGET_ODM_PROP += $(COMMON_PATH)/properties/odm.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/properties/vendor.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/properties/product.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/properties/system_ext.prop
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
